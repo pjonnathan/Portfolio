9b89bc432f76037861404a7caef488dc21481f7c
pjonnathan <jonnathanmatos846@gmail.com>
2024-02-27 22:08:00 -0300

    responsividade

--- src/componets/portifolio/portifolio.jsx
+++ src/componets/portifolio/portifolio.jsx
@@ -5,24 +5,32 @@ const Portifolio = () => {
     return (
         <div className="portifolio">
             <div className="top">
-                <div className="project">
-                    <p>Clone ChatGPT</p>
-                    <h2>React • API</h2>
-                </div>
-                <div className="project">
-                    <p>Previsão de Tempo</p>
-                    <h2>React • API</h2>
-                </div>
+                <a href="https://pjonnathan.github.io/previs-o_de_tempo/">
+                    <div className="project um">
+                        <p>Previsão de Tempo</p>
+                        <h2>React • API</h2>
+                    </div>
+                </a>
+                <a href="https://pjonnathan.github.io/PID2024/">
+                    <div className="project dois">
+                        <p>Todo List</p>
+                        <h2>Todo-List • LocalStorage</h2>
+                    </div>
+                </a>
             </div>
             <div className="bottom">
-                <div className="project">
-                    <p>Projeto 1</p>
-                    <h2>PostgresSQL • Doker</h2>
-                </div>
-                <div className="project">
-                    <p>Projeto 2</p>
-                    <h2>React • API</h2>
-                </div>
+                <a href="https://pjonnathan.github.io/Ecommerce/">
+                    <div className="project tres">
+                        <p>Ecommerce</p>
+                        <h2>React • Ecommerce</h2>
+                    </div>
+                </a>
+                <a href="https://pjonnathan.github.io/LandingPage---Agencia/">
+                    <div className="project quatro">
+                        <p>Landing Page</p>
+                        <h2>Landing Page • JavaScript</h2>
+                    </div>
+                </a>
             </div>
         </div>
     )
